80b8ae57a6cfaed4e4229fe4535967887147e6cd
Quentin Wallace <qwallace@gmail.com>
2015-07-15 22:48:25 +1000

    Latest version of Peppa Pig memory game. Added flipping.

--- app.js
+++ app.js
@@ -2,34 +2,60 @@ console.log("We're playing Peppa Pig Memory");
 
 window.onload = function(){
 
-  $(".card").on("click", function() {
+// pink, green, blue, yellow
 
-    if ( $(event.target).hasClass("picture") ) {
-      $(event.target).removeClass("picture");
+  var cards = ["danny-dog", "danny-dog", "zoe-zebra", "zoe-zebra"];
+
+  var pair = [];
+
+  var delayMs = 1000;
+
+  var chances = 2;
+
+  //_.each(cards, function(element, index) {
+    // var newCard = $("div").addClass(card);
+    // $('#game').append(newCard);
+  // });
+
+  var flipCards = function() {
+
+    console.log('It should be flipping cards...');
+
+    $('.flipped').html('');
+
+    $('.flipped').removeClass('flipped');
+
+    
+  }
+
+  $('#game div').on("click", function() {
+
+    console.log("It's clicking")
+
+
+    if ($('.flipped').length === 2) {
+      return;
+    }
+
+    if ( $(event.target).hasClass("flipped") ) {
+      $(event.target).removeClass("flipped");
+      $(event.target).html('');
     } else {
-      $(event.target).addClass("picture");
+      var content = $(event.target).attr('class');
+      $(event.target).html(content);
+      $(event.target).addClass('flipped');
+    }
+      
+    if ( $('.flipped').length === 2 && $('.flipped')[0] !== $('.flipped')[1] ) {
+      console.log('Timer starting');
+      window.setTimeout(flipCards, delayMs);
+      console.log('Timer finished');
+    } else if ( $('.flipped').length === 2 && $('.flipped')[0] === $('.flipped')[1] ) {
+      console.log("It's a pair");
+      $('.flipped').addClass('pair');
     }
 
   });
 
 
-    // $(event.target).css("background-color", "white");
-
-// **** How to do function declration in the on click event???? *****
-
-
 };
-
-// if ( $(event.target).hasClass('done') ) {
-//     $(event.target).removeClass('done');
-
-//     // using underscore
-//     _.each( $('#completed li'), function(element) {
-//       console.log(element);
-//       if (element.innerHTML === content) {
-//         $(element).remove();
-//       }
-//     });
-
-//   } else {
-//     $(event.target).addClass('done');
